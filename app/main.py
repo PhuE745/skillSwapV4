c78a4ef60a3ec0828f125a92fab2aca9ce87ba6d
PhuE745 <delfinlorenztrinidad@gmail.com>
2026-05-18 05:00:38 +0800

    adjust1

--- app/main.py
+++ app/main.py
@@ -9,7 +9,11 @@ app = FastAPI(title="SkillSwap API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://v4frontend-production.up.railway.app",
+        "http://localhost:5500",  # for local testing
+        "http://127.0.0.1:5500",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
